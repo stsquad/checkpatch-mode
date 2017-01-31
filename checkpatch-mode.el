815028716f83d38f19263ff4f3dc66849aadd136
Alex Bennée <alex.bennee@linaro.org>
2017-01-31 14:09:52 +0000

    checkpatch: bunch of cleanups
     - use correct namespace for checkpatch-run
     - add a dwim checkpatch-mode-done helper
     - add checkpatch-run-against-file helper

--- checkpatch-mode.el
+++ checkpatch-mode.el
@@ -49,6 +49,13 @@
 (make-variable-buffer-local 'checkpatch-script-path)
 (put 'checkpatch-script-path 'permanent-local t)
 
+(defvar checkpatch-result
+  "Result of last checkpatch call."
+  nil)
+(make-variable-buffer-local 'checkpatch-result)
+(put 'checkpatch-result 'permanent-local t)
+
+
 ;; Helpers
 
 (defun checkpatch-mode-update-error-regexp ()
@@ -57,30 +64,52 @@
    'compilation-error-regexp-alist-alist checkpatch-mode-regex)
   (add-to-list 'compilation-error-regexp-alist 'checkpatch))
 
+(defun checkpatch-mode-done()
+  "Bury or destroy the checkpatch buffer"
+  (interactive)
+  (when (eq major-mode 'checkpatch-mode)
+    (if (eq checkpatch-result 0)
+        (kill-buffer)
+      (bury-buffer))))
+
 ;;; Mode magic
 (defvar checkpatch-mode-map
   (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "q") 'checkpatch-mode-done)
     (define-key map (kbd "C-c C-c") 'checkpatch-mode-done)
     map)
   "Keymap for major mode `checkpatch-mode'.")
 
 ;; Launch functions
-(defun run-checkpatch (&optional script file)
-  "Run the checkpatch script against `COMMIT'."
+(defun checkpatch-run (script file)
+  "Run the checkpatch `SCRIPT' against `FILE'."
   (interactive)
-  (unless script
-    (setq script checkpatch-script-path))
-  (unless file
-    (setq file (buffer-file-name)))
   (let ((proc-name "checkpatch")
-        (buff-name (format "checkpatch-%s" file)))
-    (start-process-shell-command
-     proc-name
-     buff-name
-     (format "%s %s" script file))
+        (buff-name (format "*checkpatch-%s*" (file-name-base file))))
     (switch-to-buffer buff-name)
     (goto-char (point-min))
+    (erase-buffer)
+    (setq checkpatch-result
+          (call-process script nil t t "-f" file))
     (checkpatch-mode)))
+
+(defun checkpatch-find-script-or-prompt ()
+  "Find checkpatch script or prompt the user if not found."
+  (interactive)
+  (if (file-exists-p checkpatch-script-path)
+      checkpatch-script-path
+    (setq checkpatch-script-path
+          (ido-read-file-name
+           "Checkpatch Script: " default-directory))))
+
+(defun checkpatch-run-against-file (&optional file)
+  "Run checkpatch against `FILE'.
+If `FILE' is not set assume it is the file of the current buffer."
+  (interactive)
+  (let ((script (checkpatch-find-script-or-prompt)))
+    (if (not file)
+        (setq file (buffer-file-name)))
+    (checkpatch-run script file)))
 
 ;; Define the mode
 ;;###autoload
